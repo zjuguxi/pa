03e5215c60bca741175b614feefeec001f8c6d0c
Alien <zjuguxi@gmail.com>
2016-07-25 16:01:39 +0800

    scrap

--- pa.py
+++ pa.py
@@ -1,6 +1,12 @@
-import urllib2
+import os
+import urllib.request
+import pandas
+cwd = os.getcwd()
 
-request = urllib2.Request('http://www.baidu.com')
-response = urllib2.urlopen(request)
+for i in list:
 
-print response.read()
+    request = urllib.request.Request('http://www.baidu.com')
+    response = urllib.request.urlopen(request)
+
+    f = open('./temp/{}', 'wb+').format(i)
+    f.write(response.read())
