00b47da19edf64489b2c5e9a02bcc3a0e15b9d5f
Alien <zjuguxi@gmail.com>
2016-07-26 14:25:57 +0800

    read excel

--- pa.py
+++ pa.py
@@ -1,8 +1,24 @@
 import os
 import urllib.request
-import pandas
+import pandas as pd
+
+# 链接池目前为空
+links = []
+
+# 得到当前path
 cwd = os.getcwd()
 
+# 打开 Excel 文档，读取链接
+xls = pd.ExcelFile('1.xls')
+df = pd.read_excel(xls, 'Releases', header = [1], index_col = ['headline'], na_value = None)
+
+
+
+
+
+print(df)
+
+'''
 for i in list:
 
     request = urllib.request.Request('http://www.baidu.com')
@@ -10,3 +26,4 @@ for i in list:
 
     f = open('./temp/{}', 'wb+').format(i)
     f.write(response.read())
+'''
