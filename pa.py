c99a40f26f85261465728e0500dd45b6109d6d72
Alien <zjuguxi@gmail.com>
2016-07-28 11:57:12 +0800

    add

--- pa.py
+++ pa.py
@@ -2,22 +2,26 @@ import os
 import urllib.request
 import pandas as pd
 
-# 链接池目前为空
-links = []
+# 链接池dict目前为空
 
+link1 = {}
+link1['headline1'] = 'South China sea'
+link1['View Realease on'] = 'http://'
+links = [link1]
 # 得到当前path
 cwd = os.getcwd()
 
 # 打开 Excel 文档，读取链接
 xls = pd.ExcelFile('1.xls')
-df = pd.read_excel(xls, 'Releases', header = [1], index_col = ['headline'], na_value = None)
+df = pd.read_excel(xls, 'Releases', header = [1], index_col = [1], na_value = None)
 
-
+# 每个链接生成字典，包含『Headline』和『View Release on』两项，然后加入列表 links[]
 
 
 
 print(df)
 
+# 循环爬取链接并保存本地
 '''
 for i in list:
 
@@ -27,3 +31,5 @@ for i in list:
     f = open('./temp/{}', 'wb+').format(i)
     f.write(response.read())
 '''
+
+# 循环截图
