c5c2e079f9f08d69b2e57ecf124ff3ed402b728a
Alien <zjuguxi@gmail.com>
2016-08-27 19:35:29 +0800

    commit

--- pa_scraper.py
+++ pa_scraper.py
@@ -1,18 +1,13 @@
 # -*- coding:utf-8 -*-
 
-import requests
-import sys
+import sys, os
 import pandas as pd
 
+xls = pd.ExcelFile('3.xlsx')
+df = pd.read_excel(xls, header = None, index_col = None, na_value = None)
+
+# 获取当前路径
+cwd = os.getcwd()
 
 link = []
-
-def download_web(x):
-    ff = open('%s.html', ) % x
-    ff.writelines(x.text)
-    ff.close()
-
-for i in link:
-    r = requests.get(i)
-    download_web(r)
 
