c0049505cae24d1f75c8be171f9ae294276e79bc
Alien <zjuguxi@gmail.com>
2016-08-29 15:42:17 +0800

    auto open url and screenshot

--- pa_scraper.py
+++ pa_scraper.py
@@ -1,13 +1,31 @@
 # -*- coding:utf-8 -*-
-
-import sys, os
+import sys, os, requests
 import pandas as pd
-
-xls = pd.ExcelFile('3.xlsx')
-df = pd.read_excel(xls, header = None, index_col = None, na_value = None)
+import webbrowser as wb
+import pyscreenshot
 
 # 获取当前路径
 cwd = os.getcwd()
 
 link = []
+
+xls = pd.ExcelFile('3.xlsx')
+df = pd.read_excel(xls, header = None, index_col = None, na_value = None)
+
+rows_df = len(df.index.values.tolist())
+
+for i in range(rows_df):
+    wb.open(df.ix[i + 1, 5])
+    r = requests.head(df.ix[i, 5])
+    if r.status_code == 200
+        pyscreenshot.grab_to_file(/screenshot/%s.png) % i + 1
+    else:
+        break
+
+
+
+
+
+
+
 
